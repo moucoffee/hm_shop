7e59808c05cb298384081db1cf565bc2706089f1
moucoffee <472173778@qq.com>
2026-05-03 23:01:06 +0800

    实现轮播图效果渲染

--- lib/components/Home/Slider.dart
+++ lib/components/Home/Slider.dart
@@ -1,8 +1,11 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class Slider extends StatefulWidget {
-  Slider({Key? key}) : super(key: key);
+  final List<BannerItem> bannerList;
+  Slider({Key? key, required this.bannerList}) : super(key: key);
 
   @override
   _SliderState createState() => _SliderState();
@@ -11,14 +14,25 @@ class Slider extends StatefulWidget {
 class _SliderState extends State<Slider> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: Text("轮播图", style: TextStyle(
-        color: Colors.white,
-        fontSize: 20,
-      ),),
-    );
+    final double screanWidth = MediaQuery.of(context).size.width;
+
+    Widget _getSlider() {
+      return CarouselSlider(
+        items: List.generate(widget.bannerList.length, (index) {
+          return Image.network(
+            widget.bannerList[index].imgUrl,
+            fit: BoxFit.cover,
+            width: screanWidth,
+          );
+        }),
+        options: CarouselOptions(
+          height: 200,
+          viewportFraction: 1,
+          autoPlay: true,
+        ),
+      );
+    }
+
+    return Stack(children: [_getSlider()]);
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -4,6 +4,7 @@ import 'package:hm_shop/components/Home/Hot.dart';
 import 'package:hm_shop/components/Home/MoreList.dart';
 import 'package:hm_shop/components/Home/Slider.dart';
 import 'package:hm_shop/components/Home/Suggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -13,9 +14,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final List<BannerItem> _bannerList = [
+    BannerItem(
+      id: "1",
+      imgUrl:
+          "https://pic.rmb.bdstatic.com/bjh/news/d784e0991477b5a84f4ed1de4f1693c7.png",
+    ),
+    BannerItem(
+      id: "2",
+      imgUrl:
+          "https://img-nos.yiyouliao.com/alph/73c0cf110d2a14e92f48633db4a833c8.jpeg?yiyouliao_channel=vivo_image",
+    ),
+    BannerItem(
+      id: "3",
+      imgUrl:
+          "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg",
+    ),
+  ];
+
   List<Widget> _getScrollChildern() {
     return [
-      SliverToBoxAdapter(child: Slider()),
+      SliverToBoxAdapter(child: Slider(bannerList: _bannerList)),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
       SliverToBoxAdapter(child: Category()),
       SliverToBoxAdapter(child: SizedBox(height: 10)),
