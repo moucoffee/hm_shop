1f3d4b9db12e8161950d4d770e8c8f453cc7f851
moucoffee <472173778@qq.com>
2026-05-03 23:49:47 +0800

    轮播图的搜索框和指示导航

--- lib/components/Home/Slider.dart
+++ lib/components/Home/Slider.dart
@@ -12,12 +12,17 @@ class Slider extends StatefulWidget {
 }
 
 class _SliderState extends State<Slider> {
+  CarouselSliderController _controller = CarouselSliderController();
+  int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    //获取屏幕宽度
     final double screanWidth = MediaQuery.of(context).size.width;
 
     Widget _getSlider() {
       return CarouselSlider(
+        carouselController: _controller,
         items: List.generate(widget.bannerList.length, (index) {
           return Image.network(
             widget.bannerList[index].imgUrl,
@@ -29,10 +34,70 @@ class _SliderState extends State<Slider> {
           height: 200,
           viewportFraction: 1,
           autoPlay: true,
+          onPageChanged: (index, reason) {
+            _currentIndex = index;
+            setState(() {});
+          },
         ),
       );
     }
 
-    return Stack(children: [_getSlider()]);
+    Widget _getSearch() {
+      return Positioned(
+        left: 0,
+        right: 0,
+        top: 10,
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            height: 40,
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(0, 0, 0, 0.4),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Text(
+              "搜索",
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
+        ),
+      );
+    }
+
+    Widget _getDots() {
+      return Positioned(
+        left: 0,
+        right: 0,
+        bottom: 10,
+        child: SizedBox(
+          height: 40,
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: List.generate(widget.bannerList.length, (index) {
+              return GestureDetector(
+                onTap: () {
+                  _controller.jumpToPage(index);
+                },
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  height: 6,
+                  width: _currentIndex == index ? 40 : 20,
+                  decoration: BoxDecoration(
+                    color: _currentIndex == index ? Colors.white : 
+                    Color.fromRGBO(0, 0, 0, 0.3),
+                    borderRadius: BorderRadius.circular(3),
+                  ),
+                ),
+              );
+            }),
+          ),
+        ),
+      );
+    }
+
+    return Stack(children: [_getSlider(), _getSearch(), _getDots()]);
   }
 }
